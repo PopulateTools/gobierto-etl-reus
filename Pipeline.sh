9ac817f7c55da95051370f863144a485cf6f73dd
Fernando Blat <fernando@blat.es>
2019-12-09 19:22:46 +0100

    2020 data

--- Pipeline.sh
+++ Pipeline.sh
@@ -12,20 +12,24 @@ WORKING_DIR=/tmp/reus
 REUS_INE_CODE=43123
 
 # Years variable
-YEARS=( 2018 2019 )
+YEARS=( 2018 2019 2020 )
 
 # Data files
 EXPENSES_PLANNED_URL[2018]=https://opendata.reus.cat/dataset/2405134b-2af3-47b5-9cab-1cfb42b8a894/resource/e5b92612-5f9d-4a94-abbe-e6394909e532/download/8f2155cc-7c25-4781-a3ba-036bd7c25cb7.json
 EXPENSES_PLANNED_URL[2019]=https://opendata.reus.cat/dataset/2405134b-2af3-47b5-9cab-1cfb42b8a894/resource/9ca154c3-203b-43f2-8570-12abbf6327ae/download/73bdb636-0390-48f1-8d7f-388508f5d5a2.json
+EXPENSES_PLANNED_URL[2020]=http://dmzopendatatest.reus.cat/dataset/1b0b2e59-71c5-46bd-87fb-ec4faacbd707/resource/5b225c6f-0b53-4949-92d3-dad0c7e5614f/download/ajr2020creditsinicials.json
 
 EXPENSES_EXECUTED_URL[2018]=https://opendata.reus.cat/dataset/a7848204-7d13-4283-b7b4-32e02b5d2627/resource/0e743d72-99c7-4122-9a9d-9ab64d1ef308/download/d1280a6a-11be-4d3d-8d55-15393ddccb33.json
 EXPENSES_EXECUTED_URL[2019]=https://opendata.reus.cat/dataset/a7848204-7d13-4283-b7b4-32e02b5d2627/resource/5d1425cb-fcc4-4985-b5f7-4dce2ae896f2/download/4af822fb-0d3b-478d-a54a-3eadc7ceb265.json
+EXPENSES_EXECUTED_URL[2020]=http://dmzopendatatest.reus.cat/dataset/c95d4d03-88f1-4421-9e4a-85f295162f6a/resource/1eec82aa-ca0a-4fa9-b08e-2f8b549355f7/download/ajr2020seguimentdespeses.json
 
 INCOME_PLANNED_URL[2018]=https://opendata.reus.cat/dataset/1e25767b-49b9-4431-b7ba-6d12cecceac3/resource/3aaa81d6-25ca-465e-9ffc-59d2ba04de23/download/eac43498-91e9-4438-9122-9fe66459b625.json
 INCOME_PLANNED_URL[2019]=https://opendata.reus.cat/dataset/1e25767b-49b9-4431-b7ba-6d12cecceac3/resource/0dbd3c4d-ba2c-44a8-a2a1-92890f0ce758/download/ee89210c-4e1e-487f-9c95-f1fcb3fbe5e7.json
+INCOME_PLANNED_URL[2020]=http://dmzopendatatest.reus.cat/dataset/58f3884a-0f94-472a-a9ca-c95acb988b86/resource/49a253f6-e0ca-4087-97aa-0b31dd87bf2a/download/ajr2020previngressosinicials.json
 
 INCOME_EXECUTED_URL[2018]=https://opendata.reus.cat/dataset/68a21aa9-0e44-4d8e-ae73-ffdff6bac4fe/resource/ab77dad1-ddc4-4622-8e98-e35eaf6ec3f5/download/c3271b2d-1a4e-4dce-9b9f-d22f40b5fa65.json
 INCOME_EXECUTED_URL[2019]=https://opendata.reus.cat/dataset/68a21aa9-0e44-4d8e-ae73-ffdff6bac4fe/resource/2b1d6adc-b15a-4766-a3e2-27db25abd760/download/37b1baba-c07f-4459-b3bc-63f67c0444d2.json
+INCOME_EXECUTED_URL[2020]=http://dmzopendatatest.reus.cat/dataset/b3d06cf7-dbeb-4eec-9248-f09b3012afb9/resource/97ef147c-8a2b-4ef6-920b-7ec9e6345390/download/ajr2020seguimentingres.json
 
 BUDGETS_UPDATED_DATE_DATASET_URL=https://opendata.reus.cat/api/3/action/package_show?id=seguiment-pressupostari-de-despeses-de-l-ajuntament-de-reus
 ## End data files
@@ -72,8 +76,10 @@ for year in ${YEARS[*]}; do
   cd $REUS_ETL; ruby operations/gobierto_budgets/import-planned-budgets/run.rb $WORKING_DIR/income-planned-transformed.json $year
 
   # Load > Import planned updated budgets
-  cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/import-planned-budgets-updated/run.rb $WORKING_DIR/expenses-planned-updated-transformed.json $year
-  cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/import-planned-budgets-updated/run.rb $WORKING_DIR/income-planned-updated-transformed.json $year
+  if [ $year -ne 2020 ]; then
+	  cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/import-planned-budgets-updated/run.rb $WORKING_DIR/expenses-planned-updated-transformed.json $year
+	  cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/import-planned-budgets-updated/run.rb $WORKING_DIR/income-planned-updated-transformed.json $year
+  fi
 
   # Load > Import executed budgets
   cd $REUS_ETL; ruby operations/gobierto_budgets/import-executed-budgets/run.rb $WORKING_DIR/expenses-executed-transformed.json $year
